bf1c1aca5ce319d0ca067e54087be72cb33590f2
Mike Tigas <mike@tig.as>
2009-09-07 23:35:09 -0500

    Explicitly point to the settings file.

--- django_server/init_path.sh
+++ django_server/init_path.sh
@@ -8,3 +8,5 @@ export PATH=$PATH:/c/Python26
 export PATH=$DJANGO_SERVER_DIR/third_party/django/bin:$PATH
 export PYTHONPATH=$DJANGO_SERVER_DIR/code:$DJANGO_SERVER_DIR/third_party:$PYTHONPATH
 export PYTHONOPTIMIZE=2
+
+export DJANGO_SETTINGS_MODULE=server.settings
