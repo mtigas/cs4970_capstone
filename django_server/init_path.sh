92418abcadfd3807430e08eeedcc271ab647236b
Mike Tigas <mike@tig.as>
2009-10-05 01:49:00 -0500

    Fix newlines in the init script

--- django_server/init_path.sh
+++ django_server/init_path.sh
@@ -7,12 +7,10 @@ export PATH=$PATH:/c/Python26
 
 export PATH=$DJANGO_SERVER_DIR/third_party/django/bin:$PATH
 export PYTHONPATH=$DJANGO_SERVER_DIR/server:$DJANGO_SERVER_DIR/third_party:$PYTHONPATH
-export PYTHONOPTIMIZE=2
+export PYTHONOPTIMIZE=1
 
 export DJANGO_SETTINGS_MODULE=capstone.settings
 
 # Kill outdated Python temp files 
 alias cleanpy='find $DJANGO_SERVER_DIR -name "*.pyc" -delete;find $DJANGO_SERVER_DIR -name "*.pyo" -delete'
 cleanpy
-
-#edit file
