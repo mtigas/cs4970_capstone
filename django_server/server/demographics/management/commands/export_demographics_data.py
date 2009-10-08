ddaa1298ad82dc534eb8e12724457526a62ad641
Mike Tigas <mike@tig.as>
2009-10-08 16:39:23 -0500

    Smarter exporter

--- django_server/server/demographics/management/commands/export_demographics_data.py
+++ django_server/server/demographics/management/commands/export_demographics_data.py
@@ -9,12 +9,25 @@ import os
 import gc
 import bz2
 
-def create_export(filename,model_qs_or_iterator):
+def create_export(filename,qs):
     s = serializers.get_serializer("xml")()
     f = bz2.BZ2File(filename+".bz2",'w')
-    s.serialize(model_qs_or_iterator, ensure_ascii=False, stream=f, indent=4)
+    s.serialize(qs.iterator(), ensure_ascii=False, stream=f, indent=4)
     f.close()
     gc.collect()
+
+def split_export(base_filename,qs,split_into):
+    num_objs = qs.count()
+    per_set = int(num_objs/split_into)
+    for i in range(0,split_into):
+        start = per_set*i
+        if i == split_into-1:
+            end = num_objs
+        else:
+            end = per_set*(i+1)
+        print "  %02d/%02d (%d to %d out of %d)..."%(i+1, split_into, start, end, num_objs)
+        create_export(base_filename%ascii_uppercase[i],qs[start:end])
+        gc.collect()
 
 class Command(NoArgsCommand):
     help = "Creates fixtures for important Demographics tables."
@@ -24,30 +37,14 @@ class Command(NoArgsCommand):
         county_type = ContentType.objects.get(app_label="places",model="county")
         zipcode_type = ContentType.objects.get(app_label="places",model="zipcode")
         
-        print "Exporting DataSources and State data..."
-        create_export("4-demographics-A_datasources-state.xml",
-            list(DataSource.objects.all()) + list(PlacePopulation.objects.filter(place_type=state_type))
-        )
-        gc.collect()
+        print "Saving Datasources..."
+        create_export("/Users/mtigas/Desktop/fixtures/4-datasource.xml",DataSource.objects.all())
         
-        print "Exporting County data..."
-        create_export("4-demographics-B_county.xml",PlacePopulation.objects.filter(place_type=county_type).iterator())
-        gc.collect()
+        print "Saving State demographics..."
+        create_export("/Users/mtigas/Desktop/fixtures/5-state-data.xml",PlacePopulation.objects.filter(place_type=state_type))
+
+        print "Saving County demographics..."
+        create_export("/Users/mtigas/Desktop/fixtures/6-county-data.xml",PlacePopulation.objects.filter(place_type=county_type))
         
-        print "Exporting ZipCode data..."
-        create_export("4-demographics-C_zipcode.xml",PlacePopulation.objects.filter(place_type=zipcode_type).iterator())
-        """
-        qs = PlacePopulation.objects.filter(place_type=zipcode_type)
-        split_into = 4
-        num_objs = qs.count()
-        per_set = int(num_objs/split_into)
-        for i in range(0,split_into):
-            print "  %02d/%02d..."%(i+1, split_into)
-            start = per_set*i
-            if i == split_into-1:
-                end = num_objs
-            else:
-                end = per_set*(i+1)
-            create_export("4-demographics-C_zipcode%s.xml"%ascii_uppercase[i],qs[start:end].iterator())
-            gc.collect()
-        """
+        print "Saving ZipCode demographics..."
+        split_export("/Users/mtigas/Desktop/fixtures/7-zipcode-data-%s.xml",PlacePopulation.objects.filter(place_type=zipcode_type),8)
--- django_server/server/places/management/commands/export_places_data.py
+++ django_server/server/places/management/commands/export_places_data.py
@@ -7,31 +7,33 @@ import os
 import gc
 import bz2
 
-def create_export(filename,model_qs_or_iterator):
+def create_export(filename,qs):
     s = serializers.get_serializer("xml")()
     f = bz2.BZ2File(filename+".bz2",'w')
-    s.serialize(model_qs_or_iterator, ensure_ascii=False, stream=f, encoding="utf8", indent=4)
+    s.serialize(qs.iterator(), ensure_ascii=False, stream=f, indent=4)
     f.close()
     gc.collect()
+
+def split_export(base_filename,qs,split_into):
+    num_objs = qs.count()
+    per_set = int(num_objs/split_into)
+    for i in range(0,split_into):
+        start = per_set*i
+        if i == split_into-1:
+            end = num_objs
+        else:
+            end = per_set*(i+1)
+        print "  %02d/%02d (%d to %d out of %d)..."%(i+1, split_into, start, end, num_objs)
+        create_export(base_filename%ascii_uppercase[i],qs[start:end])
+        gc.collect()
 
 class Command(NoArgsCommand):
     help = "Creates fixtures for important Places tables."
 
     def handle_noargs(self, **options):
-        create_export("1-state.xml",State.pobjects.iterator())
-        create_export("2-county.xml",County.pobjects.iterator())
-        gc.collect()
-        
-        qs = ZipCode.pobjects.all()
-        split_into = 6
-        num_objs = qs.count()
-        per_set = int(num_objs/split_into)
-        for i in range(0,split_into):
-            print "  %02d/%02d..."%(i+1, split_into)
-            start = per_set*i
-            if i == split_into-1:
-                end = num_objs
-            else:
-                end = per_set*(i+1)
-            create_export("3-zipcode-%s.xml"%ascii_uppercase[i],qs[start:end].iterator())
-            gc.collect()
+        print "Saving States..."
+        create_export("/Users/mtigas/Desktop/fixtures/1-state.xml",State.pobjects.all())
+        print "Saving Counties..."
+        split_export("/Users/mtigas/Desktop/fixtures/2-county-%s.xml",County.pobjects.all(),3)
+        print "Saving ZipCodes..."
+        split_export("/Users/mtigas/Desktop/fixtures/3-zipcode-%s.xml",ZipCode.pobjects.all(),16)
