652d569fdfa31b85fb2adc3d11b6198139c3b39e
Mike Tigas <mike@tig.as>
2009-10-09 18:42:01 -0500

    Move the cache up one loop so that the queries that get the place aren't repeated.

--- django_server/server/graphs/views.py
+++ django_server/server/graphs/views.py
@@ -18,37 +18,57 @@ def race_piechart(request):
     Generates a png pie chart of the given location's racial breakdown. See
     graphutil.generate_race_piechart for the chart-generation bits.
     """    
-    
-    # Needs a 'place_type' request var.
-    if (not request.REQUEST.has_key('place_type')) or (not request.REQUEST['place_type'] in ['state','county','zipcode']):
+    # Check the 'place_type' GET variable.
+    place_type = request.GET.get('place_type',None)
+    if (not place_type) or (not place_type in ['state','county','zipcode']):
         return HttpResponse('"place_type" needs to be set', mimetype="text/plain")
-    
-    place = None
-    if request.REQUEST['place_type'] == "zipcode":
-        # Getting zipcode from the 'place_id' request var
-        if request.REQUEST.has_key('id'):
-            place = ZipCode.objects.get(id=request.REQUEST['id'])
-    elif request.REQUEST['place_type'] == "state":
-        # Getting state from 'abbr'
-        if request.REQUEST.has_key('abbr'):
-            place = State.objects.get(abbr__iexact=request.REQUEST['abbr'])
-        # Getting state by 'name'
-        elif request.REQUEST.has_key('name'):
-            place = State.objects.get(name__iexact=request.REQUEST['name'])
-    elif request.REQUEST['place_type'] == "county":
-        # County requires both 'state_abbr' and 'name'
-        if request.REQUEST.has_key('state__abbr') and request.REQUEST.has_key('name'):
-            place = County.objects.get(name__iexact=request.REQUEST['name'],state__abbr__iexact=request.REQUEST['state__abbr'])
-    if not place:
-        return HttpResponse("A place object with the given request string could not be found.", mimetype="text/plain")
-    
-    # Check if we have a cache of this render, with the same parameters ...
+
+    # Get the rest of the request variables that we want.
+    place_id = request.GET.get('place_id',None)
+    abbr = request.GET.get('abbr',None)
+    name = request.GET.get('name',None)
+    state_abbr = request.GET.get('state__abbr',None)
+    state_name = request.GET.get('state__name',None)
+
+    # Check if we have a cache of this render, with the same request parameters ...
     # Set the cache based on the specific object we got (place_type + place_id)
-    cache_key = "race_piechart place_type=%s place_id=%s" % (request.REQUEST['place_type'], place.pk)
+    cache_key = "race_piechart place_type=%s place_id=%s abbr=%s name=%s state_abbr=%s state_name=%s" % (
+        place_type, place_id, abbr, name, state_abbr, state_name
+    )
     response = safe_get_cache(cache_key)
-    
-    # If it wasn't cached, generate it, render it to a PNG, and cache that HTTP response.
+
+    # If it wasn't cached, do all of this fancy logic and generate the image as a PNG
     if not response:
+        place = None
+        if (place_type == "zipcode") and place_id:
+            # Get zipcode by place_id (which is the ZIP #)
+            try:
+                place = ZipCode.objects.get(id=place_id)
+            except ZipCode.DoesNotExist:
+                place = None
+        elif place_type == "state":
+            # State requires either name or abbr
+            try:
+                if abbr:
+                    place = State.objects.get(abbr__iexact=abbr)
+                elif name:
+                    place = State.objects.get(name__iexact=name)
+            except State.DoesNotExist:
+                place = None
+        elif (place_type == "county") and name:
+            # County requires name and either state__name or state__abbr
+            try:
+                if state_abbr:
+                    place = County.objects.get(name__iexact=name,state__abbr__iexact=state_abbr)
+                elif state_name:
+                    place = County.objects.get(name__iexact=name,state__name__iexact=state_name)
+            except County.DoesNotExist:
+                place = None
+    
+        # If we failed to get a location
+        if not place:
+            return HttpResponse("A %s object with the given request string could not be found." % place_type, mimetype="text/plain")
+
         # Generate the chart
         fig = generate_race_piechart(place)
         
@@ -59,9 +79,9 @@ def race_piechart(request):
         
         # save this to the cache.
         safe_set_cache(cache_key,response,86400)
-        print "Saved to cache: %s" % cache_key
-    else:    
-        print "Got from cache: %s" % cache_key
+        print "\nSaved to cache:\n\t%s\n" % cache_key
+    else:        
+        print "\nGot from cache:\n\t%s\n" % cache_key
     
     # Return the response that was either cached OR generated just now.
     return response
