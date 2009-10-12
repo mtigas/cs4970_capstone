615f32b88a3231d5c885ef92ca1781b42112b59c
Mike Tigas <mike@tig.as>
2009-10-12 15:27:14 -0500

    Add a utility that allows calling a Python function without holding up Django's response. (Say, you want to process something for the next user request. Or do something cron-like on each request, that has nothing to do with the actual response being returned to the user.)

---
+++ django_server/third_party/threadutil/__init__.py
@@ -0,0 +1,47 @@
+# coding=utf-8
+"""
+Provides a call_in_bg wrapper function that allows calling a function
+in the background of a Django view. When used in a view, the response
+can be returned while the given task continues to operate.
+"""
+from threading import Thread
+
+def db_threadsafe(func):
+    """
+    Close active Django DB connection inherited from parent
+    (forces the next DB request to create a new connection).
+    Allows multiple child threads/processes to access DB
+    concurrently since they will all have to create their own
+    separate connections to the DB.
+    """
+    from django import db
+    
+    def wrapped_func(*args, **kwargs):
+        try:
+            db.connection.close()
+        except db.InterfaceError:
+            pass
+        return func(*args, **kwargs)
+    
+    return wrapped_func
+
+def call_in_bg(function,args=[],kwargs={}):
+    """
+    Similar API to Python's threading.Thread.
+    Calls function in the background. Wraps function in a decorator that
+    causes Django DB connection (inherited from parent) to close, so that
+    the child creates it's own connection -- this is utterly required for
+    thread/process safety since the parent DB connection cannot be shared.
+    
+    Would use multiprocessing.Process, but Process appears to block the calling
+    thread, no matter what, which defeats any attempt at pushing tasks to
+    the background.
+    """
+    thread = Thread(
+        target=db_threadsafe(function),
+        args=args,
+        kwargs=kwargs
+    )
+    thread.daemon = False
+    thread.start()
+    return None
