344993fd41b5dfbb2fece06a4496a17432a5b93c
Mike Tigas <mike@tig.as>
2009-10-14 02:02:42 -0500

    Use pre-caches to optimize the "random place" view.
    When user hits random_place, the user's destination is pre-determined and cached. User goes to that destination; view triggers background task that gets a new random place and seeds the cache with that.
    
    When seeding the cache, we also trigger the place's view (which triggers the view's cache).

--- django_server/server/nationbrowse/places/views.py
+++ django_server/server/nationbrowse/places/views.py
@@ -16,29 +16,81 @@ from django.db.models.loading import get_model
 
 from nationbrowse.graphs.views import render_graph
 from threadutil import call_in_bg
+from traceback import print_exc
+import sys
+
+def seed_next_random():
+    """
+    Generates a redirect view to a random Place object (State, ZipCode, or County)
+    and caches it. Picking a random place is expensive on the DB and CPU since there
+    are over 40000 objects that it picks from, which strains the DB (since it causes
+    an iteration over the objects to select the ID).
+    
+    See random_place() below, for notes on usage.
+    """
+    cache_key = "random_place"
+    
+    print >> sys.stderr, "PRE-CACHING random_place"
+    
+    response = None
+    while not response:
+        try:
+            place_type = rand_choice(['state','county','zipcode'])
+            PlaceClass = get_model("places",place_type)
+            rand_id = rand_choice(PlaceClass.objects.order_by().values_list('pk'))[0]
+            place = PlaceClass.objects.get(pk=rand_id)
+
+            if place_type == "county":
+                response = HttpResponsePermanentRedirect(
+                    reverse("places:county_detail",args=(place.state.abbr.lower(),urlencode(place.name.lower())),current_app="places")
+                )
+                # Pre-cache this random view in the background, too.
+                call_in_bg(county_detail,(None,place.state.abbr.lower(),urlencode(place.name.lower())))
+            else:
+                response = HttpResponseRedirect(
+                    reverse("places:place_detail",args=(place_type,place.slug),current_app="places")
+                )
+                # Pre-cache this random view in the background, too.
+                call_in_bg(place_detail,(None,place_type,place.slug))
+        except:
+            print_exc()
+            response = None
+    print >> sys.stderr, "PRE-CACHED random_place: %s" % getattr(response,'Location','')
+    safe_set_cache(cache_key,response,604800)
+    
+    return response
 
 @never_cache
 def random_place(request):
-    place_type = rand_choice(['state','county','zipcode'])
-    PlaceClass = get_model("places",place_type)
-    if not PlaceClass:
-        raise Http404
+    """
+    If a random place is in the cache, use it and return that to the user.
+    If not, generate one right now.
     
-    rand_id = rand_choice(PlaceClass.objects.order_by().values_list('pk'))[0]
-    place = PlaceClass.objects.get(pk=rand_id)
+    Before returning to the user, queue up a background task that generates
+    the next random place, to save DB/CPU usage when responding to user. (Prevents
+    this view from locking up while Django picks a suitable random object.)
+    """
+    cache_key = "random_place"
+    response = safe_get_cache(cache_key)
+    
+    print >> sys.stderr, "CALL: random_place()"
+
+    if not response:
+        print >> sys.stderr, "GENERATING (was not in cache) random_place"
+        response = seed_next_random()
     
     # THIS IS AWESOME: start pre-generating the race pie chart for this place before the user
     # even sees the page
     #call_in_bg(render_graph,(None,place_type,place.slug,"race_pie",200))
     
-    if place_type == "county":
-        return HttpResponsePermanentRedirect(
-            reverse("places:county_detail",args=(place.state.abbr.lower(),urlencode(place.name.lower())),current_app="places")
-        )
-    else:
-        return HttpResponseRedirect(
-            reverse("places:place_detail",args=(place_type,place.slug),current_app="places")
-        )
+    # Pre-generate the next random location.
+    call_in_bg(seed_next_random)
+    
+    print >> sys.stderr, "RETURN: random_place()"
+    
+    return response
+    
+
     
 def place_detail(request,place_type,slug):
     """
@@ -51,6 +103,8 @@ def place_detail(request,place_type,slug):
     
     # If it wasn't cached, do all of this fancy logic and generate the image as a PNG
     if not response:
+        print >> sys.stderr, "GENERATING (was not in cache) %s" % cache_key
+        
         PlaceClass = get_model("places",place_type)
         if not PlaceClass:
             raise Http404        
@@ -78,7 +132,8 @@ def place_detail(request,place_type,slug):
         },context_instance=RequestContext(request))
         
         safe_set_cache(cache_key,response,86400)
-    
+    else:
+        print >> sys.stderr, "IN CACHE %s" % cache_key
     return response
 
 def county_detail(request,state_abbr,name):
@@ -86,6 +141,8 @@ def county_detail(request,state_abbr,name):
     response = safe_get_cache(cache_key)
     
     if not response:
+        print >> sys.stderr, "GENERATING (was not in cache) %s" % cache_key
+        
         place = get_object_or_404(County,state__abbr__iexact=state_abbr,name__iexact=name)
         
         #call_in_bg(render_graph,(None,place_type,place.slug,"race_pie",200))
@@ -99,5 +156,11 @@ def county_detail(request,state_abbr,name):
         },context_instance=RequestContext(request))
 
         safe_set_cache(cache_key,response,86400)
+        
+        # It's likely that the user will go to the State's page from here (since it's linked
+        # from the County detail page). Call it right now to pre-cache it.
+        call_in_bg(place_detail,(None,"state",place.state.slug))
+    else:
+        print >> sys.stderr, "IN CACHE %s" % cache_key
 
     return response
