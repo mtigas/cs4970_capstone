a59d88239d8a602f3afad5b37514bf15e07bc550
Mike Tigas <mike@tig.as>
2009-10-14 01:18:21 -0500

    Version of profiler that uses cProfile

--- django_server/server/nationbrowse/profiler.py
+++ django_server/server/nationbrowse/profiler.py
@@ -1,49 +1,42 @@
 import sys
 import tempfile
-import hotshot
-import hotshot.stats
+import cProfile
+import pstats
 from django.conf import settings
 from cStringIO import StringIO
 
 class ProfileMiddleware(object):
-    """
-    Displays hotshot profiling for any view.
-    http://yoursite.com/yourview/?prof
-
-    Add the "prof" key to query string by appending ?prof (or &prof=)
-    and you'll see the profiling results in your browser.
-    It's set up to only be available in django's debug mode,
-    but you really shouldn't add this middleware to any production configuration.
-    * Only tested on Linux
-    """
     def process_request(self, request):
         if request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS:
+            self.profiler = cProfile.Profile()
             self.tmpfile = tempfile.NamedTemporaryFile()
-            self.prof = hotshot.Profile(self.tmpfile.name)
-
+    
     def process_view(self, request, callback, callback_args, callback_kwargs):
         if request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS:
-            return self.prof.runcall(callback, request, *callback_args, **callback_kwargs)
-
+            return self.profiler.runcall(callback, request, *callback_args, **callback_kwargs)
+    
     def process_response(self, request, response):
         if request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS:
-            self.prof.close()
+            self.profiler.create_stats()
 
             out = StringIO()
             old_stdout = sys.stdout
             sys.stdout = out
-
-            stats = hotshot.stats.load(self.tmpfile.name)
-            #stats.strip_dirs()
-            stats.sort_stats('time', 'calls')
+            
+            self.profiler.dump_stats(self.tmpfile.name)
+            
+            stats = pstats.Stats(self.tmpfile.name)
+            
+            stats.sort_stats('time')
+            stats.strip_dirs()
             stats.print_stats()
+            #print "\n\n" + ("-"*10)
+            #stats.print_callees()
 
             sys.stdout = old_stdout
-            stats_str = out.getvalue()
-
-            if response and response.content and stats_str:
-                f=open("/tmp/profiler.txt","a")
-                f.write(stats_str + "\n\n" + ("="*20) +"\n\n")
-                f.close()
-
+            
+            f=open("/tmp/profiler.txt","a")
+            f.write(request.path + "\n\n" + out.getvalue() + "\n\n" + ("="*20) +"\n\n")
+            f.close()
+        
         return response
