a83e5ec071fa2f3f73b2d22ae76d5a03ba3b4036
Mike Tigas <mike@tig.as>
2009-10-16 23:17:02 -0500

    Tidy up the settings a bit.

--- django_server/server/nationbrowse/urls.py
+++ django_server/server/nationbrowse/urls.py
@@ -9,11 +9,17 @@ urlpatterns = patterns('',
 	url(r'^$', 'django.views.generic.simple.direct_to_template', {
 		'template': 'homepage.html',
 	}),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT,
-        'show_indexes': True
-    }),
     (r'^graphs/', include('nationbrowse.graphs.urls',namespace="graphs")),
     (r'^places/', include('nationbrowse.places.urls',namespace="places")),
     (r'^admin/', include(admin.site.urls)),
 )
+
+# If Django DEBUG is disabled, don't serve the static files -- it is
+# assumed that the deployed server is handling that. (See settings.py)
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True
+        }),
+    )
