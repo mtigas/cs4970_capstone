94332aa9ae2b3763a94d9532b06b288b917d29d5
Mike Tigas <mike@tig.as>
2009-10-19 14:54:53 -0500

    Let the profiler work (when enabled) even behind load balancer proxy. We will run with the profiler disabled in production, so there's little harm in allowing it to run when the request var is set for now.

--- django_server/server/nationbrowse/profiler.py
+++ django_server/server/nationbrowse/profiler.py
@@ -4,19 +4,20 @@ import cProfile
 import pstats
 from django.conf import settings
 from cStringIO import StringIO
+import os
 
 class ProfileMiddleware(object):
     def process_request(self, request):
-        if request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS:
+        if (request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS) or (request.GET.has_key('prof')):
             self.profiler = cProfile.Profile()
             self.tmpfile = tempfile.NamedTemporaryFile()
     
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        if request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS:
+        if (request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS) or (request.GET.has_key('prof')):
             return self.profiler.runcall(callback, request, *callback_args, **callback_kwargs)
     
     def process_response(self, request, response):
-        if request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS:
+        if (request.META.get('REMOTE_ADDR','') in settings.INTERNAL_IPS) or (request.GET.has_key('prof')):
             self.profiler.create_stats()
 
             out = StringIO()
@@ -35,7 +36,8 @@ class ProfileMiddleware(object):
 
             sys.stdout = old_stdout
             
-            f=open("/tmp/profiler.txt","a")
+            profile_log = os.path.join(settings.DJANGO_SERVER_DIR, 'static', 'media', 'profiler_log.txt')
+            f=open(profile_log,"a")
             f.write(request.path + "\n\n" + out.getvalue() + "\n\n" + ("="*20) +"\n\n")
             f.close()
         
