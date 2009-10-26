7e24f7289ac560c8fc3baf12b3ece7aa7528110d
Mike Tigas <mike@tig.as>
2009-10-26 16:56:25 -0500

    Wrap test statements in a if __name__=='__main__' block

--- sandbox/googleCharts.py
+++ sandbox/googleCharts.py
@@ -1,4 +1,6 @@
-# googleCharts.py - Python wrapper for the Google chart API
+#!/usr/bin/env python
+# coding=utf-8
+""" Python wrapper for the Google chart API """
 
 def pie_chart(values, labels=None, colors=None, size=(400,200), in_3d=False):
 	"""returns a string of the url of the generated pie graph via google"""
@@ -18,11 +20,19 @@ def pie_chart(values, labels=None, colors=None, size=(400,200), in_3d=False):
 	return "http://chart.apis.google.com/chart?chco=%s&chd=t:%s&chs=%s&cht=%s&chl=%s" % \
 		(paramColors(colors), valueList[0:len(valueList)-1], ("%sx%s" % (size[0], size[1])), param3D(in_3d), paramLabels(labels))
 
-# stuff below to be removed later
-values = [234234,234324,234234,234343,234234,234]
-labels = ["White","Black","Native American","Asian","Pacific Islander","Other"]
-colors = ["#0000FF","#5555FF","#999911","#00FF00","#FF00FF","#FFFF00"]
+# Run some tests if this script is run from the command-line.
+if __name__ == "__main__":
+    print "="*20
+    print "Pie Chart"
+    print "="*20
+    
+    values = [234234,234324,234234,234343,234234,234]
+    labels = ["White","Black","Native American","Asian","Pacific Islander","Other"]
+    colors = ["#0000FF","#5555FF","#999911","#00FF00","#FF00FF","#FFFF00"]
 
-print "%s\n%s\n%s\n" % (values,labels,colors)
+    print "%s\n%s\n%s\n" % (values,labels,colors)
 
-print pie_chart(values,in_3d=True)
+    print
+    print pie_chart(values,in_3d=True)
+    print
+    print pie_chart(values,labels,colors)
