7704d177cb90a39709ca185bb784ca15545e6566
Graham Greenfield <grahamg@gmail.com>
2009-10-26 20:23:36 -0500

    added some comments, maybe add some error checking for image area later

--- sandbox/googleCharts.py
+++ sandbox/googleCharts.py
@@ -2,21 +2,26 @@
 # coding=utf-8
 """ Python wrapper for the Google chart API """
 
+"""note: The largest possible area for all charts except maps is 300,000 pixels. 
+As the maximum height or width is 1000 pixels, examples of maximum sizes are 
+1000x300, 300x1000, 600x500, 500x600, 800x375, and 375x800. Maybe add a check later for functions."""
+
 def pie_chart(values, labels=None, colors=None, size=(400,200), in_3d=False):
 	"""returns a string of the url of the generated pie graph via google"""
 	valueList = []; labelList = []; colorList = []
+	#lambda functions to check params, convert and format strings
 	param3D = lambda x: ["p3","p"][x==False]
 	paramColors = lambda x: [''.join([z for z in colorList]),'None'][x==None]
 	paramLabels = lambda x: [labelList[0:len(labelList)-3],''][x==None]
 	if colors != None: 
-		for x in colors: colorList.append("%s%%7C" % x.strip("#"))
-		colorList[len(colorList)-1] = colorList[len(colorList)-1].strip('%7C')
+		for x in colors: colorList.append("%s%%7C" % x.strip("#")) #format values for colors
+		colorList[len(colorList)-1] = colorList[len(colorList)-1].strip('%7C') #remove last seperatng value
 	if labels != None:
-		for y in labels: labelList.append(("%s%%7c" % y).replace(" ", "+"))
+		for y in labels: labelList.append(("%s%%7c" % y).replace(" ", "+")) #add seperating values, remove spaces
 	total = sum(values)
-	valueList = map(lambda x: (float(x)/float(total)*100.0), values)
-	valueList = ''.join(["%3.2f," % s for s in valueList])
-	labelList = ''.join([z for z in labelList])
+	valueList = map(lambda x: (float(x)/float(total)*100.0), values) #normalize data values
+	valueList = ''.join(["%3.2f," % s for s in valueList]) #float values to two significant figures
+	labelList = ''.join([z for z in labelList]) #list contents to a string value
 	return "http://chart.apis.google.com/chart?chco=%s&chd=t:%s&chs=%s&cht=%s&chl=%s" % \
 		(paramColors(colors), valueList[0:len(valueList)-1], ("%sx%s" % (size[0], size[1])), param3D(in_3d), paramLabels(labels))
 
