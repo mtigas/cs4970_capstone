da658b44abfee829182240881163137016d67a12
Mike Tigas <mike@tig.as>
2009-11-28 01:02:37 -0600

    Removed dependency on matplotlib.pyplot
    Adjusted scaling on histogram (so size is given in exact pixels)
    
    Testing using "auto" aspect scaling instead of 1-to-1

--- django_server/server/nationbrowse/graphs/mpl_render_g.py
+++ django_server/server/nationbrowse/graphs/mpl_render_g.py
@@ -5,7 +5,6 @@ Contains graph-rendering functions that use the matplotlib library.
 from __future__ import division
 from matplotlib.figure import Figure
 import numpy as np
-import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid import make_axes_locatable
 
 # note: the histogram will look terrable if not enough width is given to the image if there are many boxplots to be rendered in the same space
@@ -39,7 +38,7 @@ def boxplot(values, labels=None, colors=None, size=(400,200)):
     
     return fig
 
-def histogram(values, label_x=None, label_y=None, color="#00ff00", size=(400,200)):
+def histogram(values, label_x=None, label_y=None, color="#00ff00", size=(650,650)):
     """
     Values will come as a list of tuples representing (x,y) values.
     
@@ -62,13 +61,13 @@ def histogram(values, label_x=None, label_y=None, color="#00ff00", size=(400,200
     y = np.array(y_vals)
 
     # the width and height of the figure
-    fig_w = int(size[0])/40
-    fig_h = int(size[1])/40
+    fig_w = int(size[0])/100
+    fig_h = int(size[1])/100
 
     # set up the graph descriptor object and prefrences for it
-    fig = plt.figure(1, figsize=(fig_w,fig_h), dpi=100, facecolor='#ffffff', frameon=False)
+    fig = Figure(figsize=(fig_w,fig_h), dpi=100, facecolor='#ffffff', frameon=False)
 
-    axScatter = plt.subplot(111)
+    axScatter = fig.add_subplot(111)
     divider = make_axes_locatable(axScatter)
 
     # create a new axes with a height of 1.2 inch above the axScatter
@@ -82,12 +81,9 @@ def histogram(values, label_x=None, label_y=None, color="#00ff00", size=(400,200
     fig.add_axes(axHistx)
     fig.add_axes(axHisty)
 
-    # make some labels invisible
-    plt.setp(axHistx.get_xticklabels() + axHisty.get_yticklabels(), visible=False)
-
     # plot the scatterplot and set aspect ratio, grid lines
     axScatter.scatter(x, y)
-    axScatter.set_aspect(1.)
+    axScatter.set_aspect("auto")
     axScatter.grid(True)
 
     # now determine limits manually and set the bin number
@@ -147,12 +143,10 @@ if __name__ == "__main__":
     
     print
     print "Testing histogram:"
-    values = []
     x_test = np.random.random_integers(0, high=100, size=30)
     y_test = np.random.random_integers(80, high=700, size=30)
-    for foo in x_test:
-        for bar in y_test:
-            values.append((int(foo), int(bar)))
+    
+    values = zip(x_test, y_test)
     #values  = [(0,100),(15,2500),(20,3000),(25,2700),(30,2800),(35,3600),(40,4200),(45,3500),(50,2100)]
     label_x = "Age"
     label_y = "Population"
